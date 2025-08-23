4ce1e2fecb08106c5f6e2db53177f99d522b56c2
Idwer <idwer@localhost>
2025-08-23 21:18:38 +0200

    fixes from `cargo fmt`

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,14 @@ use crate::linkedin_url_to_legible_link::decode_linkedin_url;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let url = &args[1];
-    
+
     if !url.starts_with("https://www.linkedin.com/safety/go?url=http") {
-	    println!("Not a suitable link (does not start with 'https://www.linkedin.com/safety/go?url=http'), exiting");
+        println!("Not a suitable link (does not start with 'https://www.linkedin.com/safety/go?url=http'), exiting");
 
-	    std::process::exit(0);
-    }    
+        std::process::exit(0);
+    }
 
-    if let Some(s) = decode_linkedin_url(url) { println!("The actual URL is: {}", s) }
+    if let Some(s) = decode_linkedin_url(url) {
+        println!("The actual URL is: {}", s)
+    }
 }
